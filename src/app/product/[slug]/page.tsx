346f6b00d1bd3abf9e0be1185789bab8ed664322
Baijnath kumar <bk0491731@gmail.com>
2026-03-14 19:25:56 +0530

    solve error for trip

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import NavBar from "@/components/Layout/nabar";
 import ProductDetails from "@/components/ProductDetails/productDetails";
-import { mockTrips, Trip } from "@/components/welcome/Explore";
+import { mockTrips } from "@/components/welcome/Explore";
 import { notFound } from "next/navigation";
 
 type Props = {
@@ -11,9 +11,7 @@ type Props = {
 
 export default function Page({ params }: Props) {
 
-  const trip: Trip | undefined = mockTrips.find(
-    (t) => t.slug === params.slug
-  );
+ const trip = mockTrips.find((t) => t.slug === params.slug);
 
   if (!trip) return notFound();
 
